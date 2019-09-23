e91ffd88bd30b8bb129a7321f782d8439f6f52ab
puneetneu <tanwar.p@husky.neu.edu>
2019-09-22 17:01:07 -0700

    added validation and exception handling

--- PMTOOL/src/main/java/com/pun/tan/pmtool/services/ProjectService.java
+++ PMTOOL/src/main/java/com/pun/tan/pmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.pun.tan.pmtool.services;
 
 import com.pun.tan.pmtool.domain.Project;
+import com.pun.tan.pmtool.exceptions.ProjectIdException;
 import com.pun.tan.pmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,13 @@ public class ProjectService {
     private ProjectRepository pojectRepository;
 
     public Project saveOrUpdateProject(Project project) {
+       try{
+           project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+           return pojectRepository.save(project);
+       }catch (Exception e){
+           throw new ProjectIdException("Project ID -" + project.getProjectIdentifier().toUpperCase() + " Already exist");
+       }
 
-        return pojectRepository.save(project);
+
     }
 }
--- PMTOOL/src/main/java/com/pun/tan/pmtool/web/ProjectController.java
+++ PMTOOL/src/main/java/com/pun/tan/pmtool/web/ProjectController.java
@@ -2,14 +2,18 @@ package com.pun.tan.pmtool.web;
 
 
 import com.pun.tan.pmtool.domain.Project;
+import com.pun.tan.pmtool.services.ValidationError;
 import com.pun.tan.pmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,8 +22,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ValidationError errorResponse;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+
+        ResponseEntity<?> errorMap = errorResponse.ValidationError(result);
+        if(errorMap!=null) return errorMap;
+
         Project project1=projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
