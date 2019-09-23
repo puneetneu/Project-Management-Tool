a4b57ccee9800af820c587aa7e5ccd65ae5f21e0
puneetneu <tanwar.p@husky.neu.edu>
2019-09-22 21:24:53 -0700

    added project deletion and updation

--- PMTOOL/src/main/java/com/pun/tan/pmtool/services/ProjectService.java
+++ PMTOOL/src/main/java/com/pun/tan/pmtool/services/ProjectService.java
@@ -10,16 +10,43 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
 
     @Autowired
-    private ProjectRepository pojectRepository;
+    private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
        try{
            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
-           return pojectRepository.save(project);
+           return projectRepository.save(project);
        }catch (Exception e){
            throw new ProjectIdException("Project ID -" + project.getProjectIdentifier().toUpperCase() + " Already exist");
        }
 
 
     }
+
+    public Project findByProjectIdentifier(String projectId){
+
+        Project project= projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project Id - "+ projectId+ " does not exist"  );
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProject(){
+        return projectRepository.findAll();
+    }
+
+
+    public void  deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project Id - "+ projectId+ " does not exist"  );
+        }
+
+        projectRepository.delete(project);
+
+    }
+
 }
--- PMTOOL/src/main/java/com/pun/tan/pmtool/web/ProjectController.java
+++ PMTOOL/src/main/java/com/pun/tan/pmtool/web/ProjectController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -33,8 +30,38 @@ public class ProjectController {
         if(errorMap!=null) return errorMap;
 
         Project project1=projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<>(project, HttpStatus.CREATED);
+        return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
 
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+
+        Project project = projectService.findByProjectIdentifier(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectService.findAllProject();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with Id: " + projectId + " deleted successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProject(@Valid @RequestBody Project project,BindingResult result,@PathVariable Long id){
+
+        ResponseEntity<?> errorMap = errorResponse.ValidationError(result);
+        if(errorMap!=null) return errorMap;
+
+        project.setId(id);
+        Project project1=projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project1,HttpStatus.OK);
+    }
 
 }
