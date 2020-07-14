91b791afa4dccccb1af8ae22c30af64663675424
puneetneu <tanwar.p@husky.neu.edu>
2020-07-13 21:20:15 -0400

    added pnfExeption pnfeResponse

--- PMTOOL/src/main/java/com/pun/tan/pmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ PMTOOL/src/main/java/com/pun/tan/pmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -19,4 +19,11 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest webRequest){
+
+        ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
--- PMTOOL/src/main/java/com/pun/tan/pmtool/services/ProjectTaskService.java
+++ PMTOOL/src/main/java/com/pun/tan/pmtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.pun.tan.pmtool.services;
 
 import com.pun.tan.pmtool.domain.Backlog;
+import com.pun.tan.pmtool.domain.Project;
 import com.pun.tan.pmtool.domain.ProjectTask;
+import com.pun.tan.pmtool.exceptions.ProjectNotFoundException;
 import com.pun.tan.pmtool.repositories.BacklogRepository;
+import com.pun.tan.pmtool.repositories.ProjectRepository;
 import com.pun.tan.pmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,28 +20,40 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectidentifier, ProjectTask projectTask){
 
-        Backlog backlog=  backlogRepository.findByProjectIdentifier(projectidentifier);
-        projectTask.setBacklog(backlog);
-        Integer BacklogSequence=backlog.getPTsequence();
-        BacklogSequence++;
-        backlog.setPTsequence(BacklogSequence);
-        projectTask.setProjectSequence(projectidentifier+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectidentifier);
+        try{
+            Backlog backlog=  backlogRepository.findByProjectIdentifier(projectidentifier);
+            projectTask.setBacklog(backlog);
+            Integer BacklogSequence=backlog.getPTsequence();
+            BacklogSequence++;
+            backlog.setPTsequence(BacklogSequence);
+            projectTask.setProjectSequence(projectidentifier+"-"+BacklogSequence);
+            projectTask.setProjectIdentifier(projectidentifier);
 
-        if(projectTask.getPriority()==null || projectTask.getPriority()==0){
-            projectTask.setPriority(3);
+            if(projectTask.getPriority()==null || projectTask.getPriority()==0){
+                projectTask.setPriority(3);
+            }
+
+            if(projectTask.getStatus()==null || projectTask.getStatus()==""){
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        }catch(Exception e){
+            throw new ProjectNotFoundException("Project Not Found");
         }
-
-        if(projectTask.getStatus()==null || projectTask.getStatus()==""){
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public  Iterable<ProjectTask> findBackLogById (String id){
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if(project==null)
+            throw new ProjectNotFoundException("project with id :'" + id  + "'does not exist");
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
