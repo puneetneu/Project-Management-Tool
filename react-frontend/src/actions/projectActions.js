acc99cefe14212eb1e085d27e71077f5238f929d
puneetneu <tanwar.p@husky.neu.edu>
2020-07-20 21:37:23 -0400

    routes to projectBoard and addProjecttask

--- react-frontend/src/actions/projectActions.js
+++ react-frontend/src/actions/projectActions.js
@@ -1,48 +1,48 @@
 import axios from "axios";
 import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
-export const createProject = (project, history) => async dispatch => {
+export const createProject = (project, history) => async (dispatch) => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    await axios.post("http://localhost:8081/api/project", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
-      payload: {}
+      payload: {},
     });
   } catch (error) {
     dispatch({
       type: GET_ERRORS,
-      payload: error.response.data
+      payload: error.response.data,
     });
   }
 };
 
-export const getProjects = () => async dispatch => {
+export const getProjects = () => async (dispatch) => {
   const res = await axios.get("/api/project/all");
   dispatch({
     type: GET_PROJECTS,
-    payload: res.data
+    payload: res.data,
   });
 };
 
-export const getProject = (id, history) => async dispatch => {
+export const getProject = (id, history) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
-      payload: res.data
+      payload: res.data,
     });
   } catch (error) {
     history.push("/dashboard");
   }
 };
 
-export const deleteProject = id => async dispatch => {
+export const deleteProject = (id) => async (dispatch) => {
   if (window.confirm("Are sure want to delete this ?")) {
     await axios.delete(`/api/project/${id}`);
     dispatch({
       type: DELETE_PROJECT,
-      payload: id
+      payload: id,
     });
   }
 };
