714d31080df9a51f37a165e176dab37d91034a1b
puneetneu <tanwar.p@husky.neu.edu>
2020-07-22 17:35:58 -0400

    delete Project Task

--- react-frontend/src/reducers/backlogReducer.js
+++ react-frontend/src/reducers/backlogReducer.js
@@ -1,31 +1,34 @@
-import { 
-    GET_BACKLOG, 
-    GET_PROJECT_TASK, 
-    DELETE_PROJECT_TASK} from "../actions/types";
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "../actions/types";
 
-    const intitialState = {
-        project_tasks :[],
-        project_task: {}
-    }
+const intitialState = {
+  project_tasks: [],
+  project_task: {},
+};
 
-    export default function(state=intitialState ,action){
-        switch(action.type){
-            case GET_BACKLOG:
-                return{
-                    ...state,
-                    project_tasks: action.payload
-                }
-            case GET_PROJECT_TASK:
-                return{
-                    ...state,
-                    project_task: action.payload
-                }
-            case DELETE_PROJECT_TASK:
-                return {
-                    ...state
-                }    
-            default:
-                return state;    
-        }
-
-    }
+export default function (state = intitialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        project_tasks: action.payload,
+      };
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        project_task: action.payload,
+      };
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state,
+        project_tasks: state.project_tasks.filter(
+          (project_task) => project_task.projectSequence !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+}
